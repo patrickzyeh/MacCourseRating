81029bc48af973b9e15f41a23c69bcf8646d5278
patrickzyeh <patrickzyeh@gmail.com>
2024-06-29 19:13:38 -0400

    Configure PostgreSQL database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+// Initialize Express App
 
 const app = express();
 const port = 5000;
 
+// Express Middlewares
+
 app.use(bodyParser.json());
+
+// Postgres Database
+
+// ENCRYPT THESE DATA LATER
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "",
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
