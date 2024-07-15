748e6c0a42552358a560040684b02bf0807e1a69
patrickzyeh <patrickzyeh@gmail.com>
2024-07-15 17:39:14 -0400

    Add Star Rating

--- frontend/src/pages/WriteRating.jsx
+++ frontend/src/pages/WriteRating.jsx
@@ -1,4 +1,47 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import StarsForm from "../components/StarsForm";
+
 function WriteRating({ user }) {
-  return <h1>Write Rating</h1>;
+  const params = useParams();
+  const courseCode = params.id;
+
+  const [easeRating, setEaseRating] = useState(null);
+  const [practicalityRating, setPracticalityRating] = useState(null);
+  const [enjoyabilityRating, setEnjoyabilityRating] = useState(null);
+  const [overallRating, setOverallRating] = useState(null);
+
+  return (
+    <>
+      <div className="course-container">
+        <h2 className="write-title">Write a Rating for</h2>
+        <h2 className="course-code">{courseCode}</h2>
+      </div>
+      <div className="rating-page">
+        <div className="rating-form-stars">
+          <StarsForm
+            rating={"Ease"}
+            description={"How easy was this course?"}
+            setFunction={setEaseRating}
+          />
+          <StarsForm
+            rating={"Pracitcality"}
+            description={"How practical was this course?"}
+            setFunction={setPracticalityRating}
+          />
+          <StarsForm
+            rating={"Enjoyability"}
+            description={"How enjoyable was this course?"}
+            setFunction={setEnjoyabilityRating}
+          />
+          <StarsForm
+            rating={"Overall"}
+            description={"What is your overall rating of this course?"}
+            setFunction={setOverallRating}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 export default WriteRating;
